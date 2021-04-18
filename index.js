81efa43f50b352da4af8c926fce4302a7c4e6930
DKay-code <derya.kaygisiz@outlook.com>
2021-04-18 23:31:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+function writeCards(["Ada", "Brendan", "Ali"], "birthday"); {
+  for (let i = 0; i < writeCards[0].length; i++) {
+    console.log(`Thank you, ${writeCards[i]}, for the wonderful birthday gift!`);
+  }
+ 
+  return writeCards;
+}
+ 
