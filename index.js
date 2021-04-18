0e313895cccce6cf96e68e37fdf4f8e11ef75b12
Mohamad Ziada <mhd.m.ziada@gmail.com>
2021-04-18 19:35:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(stringNames,event){
+    let allMessages = []
+for (let i = 0; i < stringNames.length; i++ ) {
+
+    allMessages.push( `Thank you, ${stringNames[i]}, for the wonderful ${event} gift!`);
+}
+return allMessages;
+}
+
+
+function countDown(number){
+    while (number > 0){
+        console.log(number);
+        number -= 1;
+    }
+    console.log( number );
+}
