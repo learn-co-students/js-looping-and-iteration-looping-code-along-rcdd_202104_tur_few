920076079e5f4d0e8eae7b25f4ffa2c5d75940d9
DKay-code <derya.kaygisiz@outlook.com>
2021-04-23 23:54:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+
+let newArray=[];
 function writeCards(name, event) {
   for (let i = 0; i < name.length; i++) {
-    console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    // console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    newArray[i] = "Thank you, " + name[i] + ", for the wonderful " + event + " gift!";
   } 
 
-return writeCards;
+return newArray;
 }
 
-writeCards(["Ada", "Brendan", "Ali"], "birthday");
+writeCards([ 'Lisa', 'Kaitlin', 'Jan'], 'surprise');
 
 
-let countDown = 10;
-while (countDown >= 0) {
-  console.log(countDown--);
-}
+function countDown(){
+    let countDown = 10;
+    while (countDown > -1) {
+      console.log(countDown--)
+    }
+  }
