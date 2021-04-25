6d8519be0915007a5812b4846b530c00a4bfa06f
Buse Şentürk <buse.senturk@outlook.com>
2021-04-25 21:14:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
+/*let names = "";
+let event = "";
+function writeCards() {
+  for (let i = 0; i < names.length; i++) {
+    console.log('Thank you, ${names[i]} for the wonderful ${event[i]}  gift!');
+
+  return names;
+}
+// console.log("Thank you, " + $names[i] + " for the wonderful " + ${event} + " gift!");
+} */
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event =  "surprise";
+function writeCards(names, event) {
+    let message = []
+for (let i=0; i<names.length; i++){
+
+  message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`) ;
+  //console.log("Thank you, "+  name[i] + " for the wonderful " +  event + " gift!" )
+} return message;
+}
+//writeCards(collection);
+/*
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event= "surprise";
+function writeCards(names, event) {
+let paragraph = []
+for (let i = 0; i < names.length; i++) {
+paragraph.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+ }
+return paragraph;
+}
+
+*/
+/*
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event= "surprise";
+function writeCards(names, event) {
+for (let i = 0; i < names.length; i++) {
+   console.log(`Thank you, ${names[i]} , for the wonderful ${event} gift!`);
+ }
+}
+writeCards(names,event);
+
+*/
+
+
+
+function countDown() {
+  let numero=10;
+  while (numero>=0) {
+    console.log(numero--);
+  }
+}
+
+
+
+
+
 // Code your solutions in this file
