008892aad35661e828c10d7f3f0e5c28870dbdc9
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-09 18:45:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(arr,eventName) {
+  let arrayFile = [];
+  for(let i = 0 ; i < arr.length; i++){
+   let result = `Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`;
+   arrayFile.push(result);
+  }
+  return arrayFile;
+}
+
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+
+function countDown(num) {
+let i = num;
+while (i >= 0) {
+console.log(i);
+i--;
+  }
+}
+ 
+countDown(10);
