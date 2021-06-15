84b0c6e306fc203fca27b86e3b56438f7d34d3e9
khadijahawa <khadijahawa555@gmail.com>
2021-06-15 15:56:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function countDown (int){
+    int = 0;
+    while (int <= 10) {
+      console.log(int++);
+    }
+
+}
+function writeCards(stringNames , stringPa){
+  let text = []
+  for(let i = 0; i<stringNames.length; i++){
+      let insideText
+      insideText = `Thank you, ${stringNames[i]}, for the wonderful ${stringPa} gift!`
+      text.push(insideText)
+  }
+  return text    
+}
+
+
