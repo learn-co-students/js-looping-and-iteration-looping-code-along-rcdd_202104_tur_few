e611952a56194eae67581e316ef7b426cf2f11e8
Rahaf Shora <rahaf.it@gmail.com>
2021-06-19 19:37:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+  let arr = [];
+  for (let item of names) {
+    arr.push(`Thank you, ${item}, for the wonderful surprise gift!`);
+  }
+  return arr;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
